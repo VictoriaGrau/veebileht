97ad287162e5365e3c97cfa098efd4d6a91ad224
VictoriaGrau <victoriagrau@ut.ee>
2024-12-03 00:03:09 +0200

    final kood koos valge riba ja muude uuendustega

--- nupp.js
+++ nupp.js
@@ -7,3 +7,6 @@ function navigateToPage() {
         window.location.href = selectedPage;
     }
 }
+
+// Kood on pärit.
+// W3Schools.(). How TO -Clickable Dropdown. W3Schools. Kasutatud 05.11.2024, https://www.w3schools.com/howto/howto_js_dropdown.asp
